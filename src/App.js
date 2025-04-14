b7575234b25a828beec924ea0777f41e82dbe822
Kaile Borbon <airkaile@gmail.com>
2025-04-14 21:40:04 +0800

    updated for Budget App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Bank from './components/bank/bank.jsx'
+import Bank from './components/bank/bank.jsx';
+import BudgetPage from './pages/BudgetPage.jsx';
 
 function App() {
   return (
     <div className="App">
       <Bank />
+      <BudgetPage />
     </div>
   );
 }
